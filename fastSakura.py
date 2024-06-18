e68e1092b95a02b27a539411ce41ffc1ca0559a7
Lou ! <lou@habert.me>
2024-06-18 17:47:21 +0200

    🍒 progress ?

--- fastSakura.py
+++ fastSakura.py
@@ -13,8 +13,11 @@ import re
 import fastwsgi
 from configparser import ConfigParser
 
-RE_URL= re.compile(r"[\?\&]")
-routes={}
+RE_URL = re.compile(r"[\&]")
+RE_PARAM = re.compile(r"[\=]")
+routes = {}
+
+
 class Server:
     def __init__(self, path, configFile, noStart=False):
         print("starting Sakura server...")
@@ -29,51 +32,65 @@ class Server:
         self.start()
 
     def serve(func):
-        def wrapper(self,*args,**kwargs):
-            print("we are in the decorator")
-            headers = [('Content-Type', 'text/plain')]
-            try : 
-                res = func()
+        def wrapper(self, start_response, *args, **kwargs):
+            print("we are in the decorator", func.__name__)
+
+            try:
+                res = func(self, *args, **kwargs).encode()
+                print("res =", res)
+                # res=res
+                headers = [('Content-Type', 'text/html')]
                 start_response('200 OK', headers)
                 return res
             except Exception as e:
-                start_response('502 ERROR', headers)
-                return e
-        name = func.__name__
-        if func.__name__ != "index":
-            name = "/"
-    
-        routes[name]={"params":inspect.signature(func).parameters,"target":wrapper}
-        return wrapper
-    
-    def onrequest(self, environ, start_response):
-        print("[INFO] Sakura - request received :'", str(environ['PATH_INFO'])+"'")
-        target = re.split(RE_URL, environ['PATH_INFO'])
-        print(target)
-        if routes.get(target[0]):
-            
-            return routes[target[0]]["target"]()
-        else:
-            start_response("404 Error")
-            return ['']
+                print("AHHH", e)
+                headers = [('Content-Type', 'text/plain')]
+                start_response('500 ERROR', headers)
+                return ("ERROR 500 : " + str(e)).encode()
 
+        name = func.__name__
+        if func.__name__ == "index":
+            name = "/"
+        else:
+            name = "/" + func.__name__
+
+        routes[name] = {"params": inspect.signature(func).parameters, "target": wrapper}
+        return wrapper
+
+    def onrequest(self, environ, start_response):
+        print("[INFO] Sakura - request received :'", str(environ['PATH_INFO']) + "'")
+        target = environ['PATH_INFO']
+
+        if routes.get(target):
+            print("TARGET :", target, environ['PATH_INFO'])
+
+            if environ.get('QUERY_STRING'):
+                args = re.split(RE_URL, environ['QUERY_STRING'])
+                return routes[target]["target"](self, start_response, args)
+            else:
+                return routes[target]["target"](self, start_response)
+
+        else:
+            response_headers = [('Content-Type', 'text/plain')]
+            start_response("404 Not Found", response_headers)
+            return "ERROR 404 : Not found".encode()
 
 
     def onLogin(self, uid):
-        pass
+            pass
 
     def onStart(self):
         pass
 
     @serve
-    def foo(self,a,b="o_O"):
-        pass
+    def foo(self, a, b="o_O"):
+        return "foooo" + str(a)
 
     @serve
-    def bar(self,*args,**kwargs):
-        pass
+    def bar(self, *args, **kwargs):
+        return str(*args)
 
-    #--------------------------GENERAL USE METHODS------------------------------
+    # --------------------------GENERAL USE METHODS------------------------------
 
     def importConf(self, configFile):
         self.config = ConfigParser()
@@ -84,9 +101,11 @@ class Server:
             print("please create a config file")
 
     def start(self):
-        fastwsgi.run(wsgi_app=self.onrequest, host=self.config.get('server', 'IP'), port=int(self.config.get('server', 'PORT')))
+        fastwsgi.run(wsgi_app=self.onrequest, host=self.config.get('server', 'IP'),
+                     port=int(self.config.get('server', 'PORT')))
+
 
 class HTTPError(Exception):
     def __init__(error):
-        print("ERROR - SAKURA",str(error))
+        print("ERROR - SAKURA", str(error))
         return "unknown error"
--- server.py
+++ server.py
@@ -6,7 +6,8 @@ PATH = dirname(abspath(__file__))
 class Hello(Server):
     @Server.serve
     def index(self):
-        return open(PATH + "/ressources/home/home.html")
+        print("IN INDEX")
+        return open(PATH + "/ressources/home/home.html").read()
 
 
 Hello(path=PATH, configFile="/server.ini")
